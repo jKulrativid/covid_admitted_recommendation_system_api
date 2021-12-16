f9f7d9f2e527b4204778fdbc8a657bebf2a18f54
K. Jetnipit <jetnipit54@gmail.com>
2021-12-16 21:24:28 +0700

    update user entity

--- src/api/entities/user.go
+++ src/api/entities/user.go
@@ -1,7 +1,13 @@
 package entities
 
 type User struct {
-	UserId   uint64 `json:"userid" binding:"omitempty" gorm:"column:userid"` // omitempty for userID=0
-	UserName string `json:"username" binding:"required" gorm:"column:username"`
-	Password string `json:"password" binding:"required" gorm:"column:password"`
+	Uuid           string `gorm:"column:uuid"` // omitempty for userID=0
+	UserName       string `gorm:"column:username"`
+	HashedPassword string `gorm:"column:password"`
+	Salt           string `gorm:"column:salt"`
+}
+
+type UserLogin struct {
+	UserName string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
 }
