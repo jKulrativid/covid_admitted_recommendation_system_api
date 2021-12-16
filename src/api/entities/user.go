e3d28f6522d2bb7a4969637ce13af293c5586a00
K. Jetnipit <jetnipit54@gmail.com>
2021-12-16 20:20:11 +0700

    update User struct tag

--- src/api/entities/user.go
+++ src/api/entities/user.go
@@ -1,7 +1,7 @@
 package entities
 
 type User struct {
-	UserId   uint64 `json:"id" binding:"required"`
-	UserName string `json:"username" binding:"required"`
-	Password string `json:"password" binding:"required"`
+	UserId   uint64 `json:"userid" binding:"omitempty" gorm:"column:userid"` // omitempty for userID=0
+	UserName string `json:"username" binding:"required" gorm:"column:username"`
+	Password string `json:"password" binding:"required" gorm:"column:password"`
 }
