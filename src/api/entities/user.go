dbccd1edea95a12e2071c53404c9096f6723daf2
K. Jetnipit <jetnipit54@gmail.com>
2021-12-16 21:28:42 +0700

    update email validator in user entity

--- src/api/entities/user.go
+++ src/api/entities/user.go
@@ -3,6 +3,7 @@ package entities
 type User struct {
 	Uuid           string `gorm:"column:uuid"` // omitempty for userID=0
 	UserName       string `gorm:"column:username"`
+	Email          string `gorm:"column:email" valid:"email"`
 	HashedPassword string `gorm:"column:password"`
 	Salt           string `gorm:"column:salt"`
 }
