53b308276b7cb08b387e88c339405018ca179f70
K. Jetnipit <jetnipit54@gmail.com>
2021-12-16 21:40:12 +0700

    update user entity

--- src/api/entities/user.go
+++ src/api/entities/user.go
@@ -8,7 +8,8 @@ type User struct {
 	Salt           string `gorm:"column:salt"`
 }
 
-type UserSignIn struct {
+// for sign-in sign-out
+type UserSign struct {
 	UserName string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
@@ -17,8 +18,4 @@ type UserRegister struct {
 	UserName string `json:"username" binding:"required"`
 	Email    string `json:"email" binding:"required"`
 	Password string `json"password" binding:"required"`
-}
-
-type UserSignOut struct {
-	// TODO create user signout
 }
