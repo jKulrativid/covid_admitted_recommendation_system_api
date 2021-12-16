b2e7ec3adb2385ac933244fd5ac82c792a9c3373
K. Jetnipit <jetnipit54@gmail.com>
2021-12-16 12:48:04 +0700

    add godot env for reading env variable from .env

--- src/api/server.go
+++ src/api/server.go
@@ -8,12 +8,18 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
 )
 
 var err error
 
 func main() {
 
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+
+	}
 	databases.DB, err = gorm.Open("mysql", databases.DbURL(databases.BuildDBConfig()))
 	if err != nil {
 		log.Fatal(err)
@@ -27,9 +33,7 @@ func main() {
 		log.Fatal(err)
 
 	}
-
 	server := routers.NewRouter()
-
 	server.Run(":8080")
 
 }
