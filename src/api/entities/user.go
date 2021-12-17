bb0c49e24a6ba5d3100577c19bab5b127708f027
K. Jetnipit <jetnipit54@gmail.com>
2021-12-17 16:37:53 +0700

    update struct tag

--- src/api/entities/user.go
+++ src/api/entities/user.go
@@ -1,21 +1,21 @@
 package entities
 
 type User struct {
-	Uuid           string `gorm:"column:uuid"` // omitempty for userID=0
-	UserName       string `gorm:"column:username"`
-	Email          string `gorm:"column:email" valid:"email"`
-	HashedPassword string `gorm:"column:password"`
-	Salt           string `gorm:"column:salt"`
+	Uuid           string `gorm:"primary_key"`              // user uuid
+	UserName       string `gorm:"unique;type:varchar(32);"` // just a username
+	Email          string `gorm:"unique;type:varchar(320)"` // user email (required)
+	HashedPassword string `gorm:"type:varchar(72)"`         // user password already hashed with salt
+	Salt           string `gorm:"type:varchar(32)"`         // unique salt for every user
 }
 
 // for sign-in sign-out
-type UserSign struct {
+type UserSignIn struct {
 	UserName string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
 
 type UserRegister struct {
 	UserName string `json:"username" binding:"required"`
-	Email    string `json:"email" binding:"required"`
-	Password string `json"password" binding:"required"`
+	Email    string `json:"email" binding:"email"`
+	Password string `json:"password" binding:"required"`
 }
